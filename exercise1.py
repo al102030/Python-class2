ce0f696e1cf3bc3f0e5c8f1971692725203e9b28
Erwin <89207045+Mehradhp@users.noreply.github.com>
2024-08-14 01:56:48 +0330

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -324,7 +324,20 @@ random_name()
 
 # Mehrad
 # ================================
-# Your Code
+def cyber():
+    num = int(input("Enter a Number: "))
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz!")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+
+
+cyber()
+
 # ================================
 
 
