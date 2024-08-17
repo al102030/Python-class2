659e83dc60b204a39432d7a7878289acd8c4042f
Pantea <shahrambahramnejad@gmail.com>
2024-08-17 09:40:41 +0330

    Update exercise1.py

--- exercise1.py
+++ exercise1.py
@@ -405,7 +405,9 @@ print(power(16))
 
 # Pantea
 # ================================
-# Your Code
+def square_even_numbers(n):
+    return [x**2 for x in range(1, n+1) 
+if x % 2 == 0]
 # ================================
 
 
