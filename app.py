bc1c01ddfc9e6f7e4d8d6f2d4730fa46a97a706a
Ali Darvishi <ali.darvishi.k@gmail.com>
2024-08-17 12:26:58 +0330

    End of Data Structure

--- app.py
+++ app.py
@@ -39,8 +39,7 @@
 # fizz_buzz()
 
 def calculate_square(number):
-    my_list = list(range(1, number+1))
-    return [item**2 for item in my_list if item % 2 == 0]
+    return [item**2 for item in range(1, number+1) if item % 2 == 0]
 
 
 print(calculate_square(14))
