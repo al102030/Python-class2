3b188ac26ab90511da0251747ae8fc56fc98f2cf
Ali Darvishi <ali.darvishi.k@gmail.com>
2024-09-28 23:33:50 +0330

    Opacity

--- main.py
+++ main.py
@@ -1,67 +1,67 @@
-# import xml.dom.minidom
+import xml.dom.minidom
 
-# domtree = xml.dom.minidom.parse('people.xml')
+domtree = xml.dom.minidom.parse('people.xml')
 
-# group = domtree.documentElement
+group = domtree.documentElement
 
-# people = group.getElementsByTagName("person")
+people = group.getElementsByTagName("person")
 
-# for person in people:
-#     print(f"-- Person {person.getAttribute('id')} --")
+for person in people:
+    print(f"-- Person {person.getAttribute('id')} --")
 
-#     name = person.getElementsByTagName('name')[0].childNodes[0].nodeValue
-#     age = person.getElementsByTagName('age')[0].childNodes[0].nodeValue
-#     weight = person.getElementsByTagName('weight')[0].childNodes[0].nodeValue
-#     height = person.getElementsByTagName('height')[0].childNodes[0].nodeValue
+    name = person.getElementsByTagName('name')[0].childNodes[0].nodeValue
+    age = person.getElementsByTagName('age')[0].childNodes[0].nodeValue
+    weight = person.getElementsByTagName('weight')[0].childNodes[0].nodeValue
+    height = person.getElementsByTagName('height')[0].childNodes[0].nodeValue
 
-#     print(f"Name: {name}")
-#     print(f"Age: {age}")
-#     print(f"Weight: {weight}")
-#     print(f"Height: {height}")
+    print(f"Name: {name}")
+    print(f"Age: {age}")
+    print(f"Weight: {weight}")
+    print(f"Height: {height}")
 
-#     people[0].getElementsByTagName('name')[0].childNodes[0].nodeValue
-#     people.setAttribute("id", "200")
-#     people.setAttribute("newAttr", "Hello")
+    people[0].getElementsByTagName('name')[0].childNodes[0].nodeValue
+    people.setAttribute("id", "200")
+    people.setAttribute("newAttr", "Hello")
 
-#     domtree.writexml(open('people.xml', 'w'))
-
-
-import xml.sax
+    domtree.writexml(open('people.xml', 'w'))
 
 
-class PeopleHandler(xml.sax.ContentHandler):
-
-    def startElement(self, name, attrs):
-        self.current = name
-        if name == "person":
-            print(f"-- Person {attrs['id']} --")
-
-    def characters(self, content):
-        if self.current == "name":
-            self.name = content
-        elif self.current == "age":
-            self.age = content
-        elif self.current == "weight":
-            self.weight = content
-        elif self.current == "height":
-            self.height = content
-
-    def endElement(self, name):
-        if self.current == "name":
-            print(f"Name: {self.name}")
-        elif self.current == "age":
-            print(f"Age: {self.age}")
-        elif self.current == "weight":
-            print(f"Weight: {self.weight}")
-        elif self.current == "height":
-            print(f"Height: {self.height}")
-        self.current = ""
+# import xml.sax
 
 
-handler = PeopleHandler()
-parser = xml.sax.make_parser()
-parser.setContentHandler(handler)
-parser.parse('people.xml')
+# class PeopleHandler(xml.sax.ContentHandler):
+
+#     def startElement(self, name, attrs):
+#         self.current = name
+#         if name == "person":
+#             print(f"-- Person {attrs['id']} --")
+
+#     def characters(self, content):
+#         if self.current == "name":
+#             self.name = content
+#         elif self.current == "age":
+#             self.age = content
+#         elif self.current == "weight":
+#             self.weight = content
+#         elif self.current == "height":
+#             self.height = content
+
+#     def endElement(self, name):
+#         if self.current == "name":
+#             print(f"Name: {self.name}")
+#         elif self.current == "age":
+#             print(f"Age: {self.age}")
+#         elif self.current == "weight":
+#             print(f"Weight: {self.weight}")
+#         elif self.current == "height":
+#             print(f"Height: {self.height}")
+#         self.current = ""
+
+
+# handler = PeopleHandler()
+# parser = xml.sax.make_parser()
+# parser.setContentHandler(handler)
+# parser.parse('people.xml')
 
 # import zipfile
 # import csv
